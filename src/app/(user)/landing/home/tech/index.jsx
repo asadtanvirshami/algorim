5ede9138c123be52eb161edcdedf0f685e6227f9
Ahsan <Ahsan.dev.404@gmail.com>
2024-08-01 13:27:52 -0700

    yarn setup

--- src/app/(user)/landing/home/tech/index.jsx
+++ src/app/(user)/landing/home/tech/index.jsx
@@ -18,30 +18,44 @@ import firebase from "../../../../../../public/images/tech/firebase.png";
 import tsxc from "../../../../../../public/images/tech/ts.png";
 import git from "../../../../../../public/images/tech/git.png";
 import wordpress from "../../../../../../public/images/tech/wordpress.png";
+import btc from "../../../../../../public/images/tech/blk.png";
+import tf from "../../../../../../public/images/tech/tf.png";
+import rust from "../../../../../../public/images/tech/rust.png";
+import gcp from "../../../../../../public/images/tech/gcp.png";
+
 import Image from "next/image";
 const Tech = () => {
   return (
-    <div className="grid  justify-center  items-center ">
-      <div className="grid grid-cols-6 space-x-8">
+    <div className="flex  flex-col justify-center space-x-5 space-y-5 mt-16 items-center ">
+      <div className="flex space-x-8">
         <Image src={react} className="h-16 w-16" />
-        <Image src={py} className="h-16 w-16"  />
-        <Image src={ang} className="h-16 w-16"  />
-        <Image src={aws} className="h-16 w-16"  />
-        <Image src={azure}className="h-16 w-16"  />
-        <Image src={js}className="h-16 w-16"  />
+        <Image src={py} className="h-16 w-16" />
+        <Image src={ang} className="h-16 w-16" />
+        <Image src={aws} className="h-16 w-16" />
+        <Image src={gcp} className="h-16 w-16" />
+        <Image src={azure} className="h-16 w-16" />
+        <Image src={js} className="h-16 w-16" />
+        <Image src={tsxc} className="h-16 w-16" />
+        <Image src={rust} className="h-16 w-16" />
       </div>
-      <div className="grid grid-cols-5 justify-center items-center">
-        <Image src={sql} className="h-16 w-16"/>
-        <Image src={psql} className="h-16 w-16"/>
-        <Image src={mgdb} className="h-16 w-16"/>
-        <Image src={graph} className="h-16 w-16"/>
-        <Image src={redis} className="h-16 w-16"/>
+      <div className="flex  space-x-8 justify-center items-center">
+        <Image src={sql} className="h-16 w-16" />
+        <Image src={psql} className="h-16 w-16" />
+        <Image src={mgdb} className="h-16 w-16" />
+        <Image src={graph} className="h-16 w-16" />
+        <Image src={redis} className="h-16 w-16" />
+        <Image src={firebase} className="h-16 w-16" />
       </div>
-      <div className="grid grid-cols-4">
-        <Image src={node} className="h-16 w-16"/>
-        <Image src={vue} className="h-16 w-16"/>
-        <Image src={mgdb} className="h-16 w-16"/>
-        <Image src={mgdb} className="h-16 w-16"/>
+      <div className="flex  space-x-8 justify-center items-center">
+        <Image src={node} className="h-16 w-16" />
+        <Image src={vue} className="h-16 w-16" />
+        <Image src={tf} className="h-16 w-16" />
+        <Image src={btc} className="h-16 w-16" />
+      </div>
+      <div className="flex  space-x-8 justify-center items-center">
+        <Image src={wordpress} className="h-16 w-16" />
+        <Image src={git} className="h-16 w-16" />
+        <Image src={dart} className="h-16 w-16" />
       </div>
     </div>
   );
