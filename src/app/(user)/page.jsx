c9bb4c7f04cae0c02f32963719d0de40491c0e43
Ahsan <Ahsan.dev.404@gmail.com>
2024-09-30 03:03:51 -0700

    loading screen with settimeout

--- src/app/(user)/page.jsx
+++ src/app/(user)/page.jsx
@@ -1,18 +1,28 @@
-import Image from 'next/image'
-import React from 'react'
+"use client"
+import Image from "next/image";
+import React, { useEffect } from "react";
 
-import logo from '../../../public/a_white.png'
+import logo from "../../../public/a_white.png";
+import { useRouter } from "next/navigation";
 
 const page = () => {
+  const router = useRouter();
+  React.useEffect(() => {
+    setTimeout(() => router.push("/landing/home"), 800);
+  }, []);
   return (
-    <div className='h-screen bg-black'>
-      <div className='w-full h-full flex justify-center items-center align-middle'>
+    <div className="h-screen bg-black">
+      <div className="w-full h-full flex justify-center items-center align-middle">
         <div>
-          <Image src={logo} className='h-full w-full animate-pulse' alt='logo'/>
+          <Image
+            src={logo}
+            className="h-full w-full animate-pulse"
+            alt="logo"
+          />
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
